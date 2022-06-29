7b84f67c2a4f1d666e1e248b96e4142f3969bcd9
bdquispe <daysi.quispe444@gmail.com>
2022-06-29 10:23:39 -0400

    default commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+
+class ClientController extends Controller {
+
+	public function index() {
+		$res = Http::get('https://randomuser.me/api/');
+		return view('perfil')->with('user', $res->json());
+	}
+
+	public function getAllPosts() {
+		$res = Http::get('https://jsonplaceholder.typicode.com/posts');
+		return view('demo')->with('posts', $res->json());
+	}
+	
+	public function getPostById($id) {
+		$res = Http::get('https://jsonplaceholder.typicode.com/posts/'.$id);
+		return $res->json();
+	}
+
+	public function addPost() {
+		$req = Http::post('https://jsonplaceholder.typicode.com/posts', [ 'userId' => 1, 'title' => 'New Post title', 'body' => 'New post description' ]);
+		return $req->json();
+	}
+
+	public function updatePost($id) {
+		$req = Http::put('https://jsonplaceholder.typicode.com/posts/'.$id, [ 'title' => 'Update Post title', 'body' => 'Update post description' ]);
+		return $req->json();
+	}
+
+	public function deletePost($id) {
+		$req = Http::delete('https://jsonplaceholder.typicode.com/posts/'.$id);
+		return $req->json();
+	}
+}
